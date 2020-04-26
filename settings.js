f5e833da5e879bf70d1ac59e3e0e5ad814d3fc38
oscar-raig <oscar.raig@gmail.com>
2020-04-26 17:05:20 +0200

    Initial SetUp

---
+++ settings.js
@@ -0,0 +1,4 @@
+module.exports = {
+  REGION: 'eu-west-1',
+  BUCKET: 'kikilo.org/dictionaries'
+}
---
+++ src/uploadS3.js
@@ -0,0 +1,33 @@
+const REGION = require('../settings').REGION
+const BUCKET = require('../settings').BUCKET
+
+const s3FolderUpload = require('s3-folder-upload')
+
+
+const {AWS_ACCESS_KEY, AWS_SECRET_KEY} = process.env
+
+const credentials = {
+  accessKeyId: AWS_ACCESS_KEY,
+  secretAccessKey: AWS_SECRET_KEY,
+  region: REGION,
+  bucket: BUCKET
+}
+
+
+const options = {
+  useFoldersForFileTypes: false,
+  useIAMRoleCredentials: false
+}
+
+const filesOptions = {
+  'dictionary.json': {
+    CacheControl: 'public, max-age=3600'
+  }
+}
+
+const invalidations = undefined
+
+module.exports = function upload() {
+  s3FolderUpload('dist', credentials, options, invalidations, filesOptions)
+  console.log('deploying>>')
+}
